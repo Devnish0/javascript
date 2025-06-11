e21fd0071af09661393f04a1d505c796ccd93eeb
nishhhhh <156004996+Devnish0@users.noreply.github.com>
2025-06-11 16:39:36 +0530

    start javascript
    starting the javascript journey

---
+++ 01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 123312; //always use const as aconstant//
+let accountEmail = "nishank@gmail.com"; //always use let as a variable
+var accountcity = "banglore"; //do not use var as it has some problem with scope
+accountname = "nishank"; //bad way to type any code
+
+//accountId = 123123; // cannot change the const if changed it will throw an error
+accountEmail = "nishankgangwar@gmail.com";
+accountcity = "bareilly";
+// never ever use var as a variable as it has some problem with the block scope and functional scope
+
+accountname = "gangwar";
+
+/*console.log(accountEmail);
+console.log(accountId);
+console.log(accountcity);
+console.log(accountname);*/
+//can also use console table for mass prints at once for example *** use {{ brackets in table console}}
+console.table({ accountEmail, accountId, accountcity, accountname });
